eb8c5a00c572dbb2698b58d1b19a417169a55243
BurakKorkkmaz <98706309+BurakKorkkmaz@users.noreply.github.com>
2022-01-30 20:37:39 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+islem = str(input("işlem seçiniz: "))
+print("toplama işlemi 1")
+if  islem == "1":
+    sayi1 = int(input("sayi1 giriniz: "))
+    sayi2 = int(input("sayi2 giriniz: "))
+    print("sonuc:", sayi1 + sayi2)
+print("çıkarma işlemi 2")
+
+if  islem == "2":
+    sayi1 = int(input("sayi1 giriniz: "))
+    sayi2 = int(input("sayi2 giriniz: "))
+    print("sonuc:", sayi1 - sayi2)
+print("çarpma işlemi 3")
+
+if  islem == "3":
+	  sayi1 = int(input("sayi1 giriniz"))
+	  sayi2 = int(input("sayi2 giriniz"))
+	  print("sonuc:", sayi1 * sayi2)
+print("bölme işlemi 3")
+
+if  islem == "4":
+	  sayi1 = int(input("sayi1 giriniz"))
+	  sayi2 = int(input("sayi2 giriniz"))
+	  print("sonuc:", sayi1 / sayi2)
